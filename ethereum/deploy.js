9e0e7c49637d04be353e03bb8f3f0445325cd698
Nikhil Asrani <asrani.nikhil@gmail.com>
2021-06-06 14:22:16 +0530

    Initial commit

---
+++ ethereum/deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+const dotenv = require("dotenv").config();
+
+const seed_phase = process.env.SEED_PHASE;
+const network_url = process.env.NETWORK_URL;
+
+const provider = new HDWalletProvider(seed_phase, network_url);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+};
+deploy();
