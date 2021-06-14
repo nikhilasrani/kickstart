42cf5c344c39b22f92722eb2961753623615725e
Nikhil Asrani <asrani.nikhil@gmail.com>
2021-06-14 21:08:02 +0530

    initialized index page and set up web3 providers

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,7 +1,16 @@
 import Web3 from "web3";
- 
-window.ethereum.request({ method: "eth_requestAccounts" });
- 
-const web3 = new Web3(window.ethereum);
+let web3;
+
+// const dotenv = require("dotenv").config();
+const network_url = process.env.NETWORK_URL;
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+  // We are in the browser and metamask is running.
+  window.ethereum.request({ method: "eth_requestAccounts" });
+  web3 = new Web3(window.ethereum);
+} else {
+  // We are on the server *OR* the user is not running metamask
+  const provider = new Web3.providers.HttpProvider(network_url);
+  web3 = new Web3(provider);
+}
  
 export default web3;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,20 @@
-export default () => {
-    return <h1>Index page</h1>
-}
+import {useState, useEffect} from 'react'
+import factory from '../ethereum/factory';
 
+
+const CampaignIndex = () => {
+    const [campaigns, setCampaigns] = useState([]);
+    useEffect(()=> {
+        const getDeployedCampaigns = async () => {
+            const campaigns = await factory.methods.getDeployedCampaigns().call();
+            setCampaigns(campaigns)
+        }
+         if(process.browser) getDeployedCampaigns();
+    },[])
+
+    console.log(campaigns)
+    return <h1>Index page</h1>
+};
+
+
+export default CampaignIndex
